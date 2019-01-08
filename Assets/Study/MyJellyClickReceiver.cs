2a1d3fa735a9e1d7a86699885c08a8db336ad1c2
imanishi <satoshi.imanishi@gmail.com>
2019-01-09 04:57:03 +0900

    PART 4: INTEGRATION WITH UNITY

---
+++ Assets/Study/MyJellyClickReceiver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyJellyClickReceiver : MonoBehaviour {
+
+    Renderer modelRenderer; // MeshRendererのComponentをキャッシュする
+    float controlTime;
+
+    private void Start()
+    {
+        modelRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        controlTime += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0)) controlTime = 0;
+        modelRenderer.material.SetFloat("_ControlTime", controlTime);
+    }
+
+}
